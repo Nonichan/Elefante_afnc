c46abd142d29867ad0a1a1d20c56809505ead257
Nonichan <angelnonigo02@gmail.com>
2022-03-20 23:10:05 -0600

    Practica 7: Completada

--- app/src/main/java/com/example/elefante_afnc/MainActivity.java
+++ app/src/main/java/com/example/elefante_afnc/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.elefante_afnc;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -52,17 +55,35 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.fragment_huesped);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(this,"Seleccionaste configurar", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.options_sugerir:
+                Toast.makeText(this,"Seleccionaste sugerir", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.options_ayuda:
+                Toast.makeText(this,"Seleccionaste ayuda", Toast.LENGTH_SHORT).show();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+        }
     }
 }
